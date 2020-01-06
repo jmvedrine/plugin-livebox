495e0d47cafba078ddc17bcf07a04213240f76d9
Jean-Michel Vedrine <vedrine@vedrine.org>
2020-01-06 15:53:42 +0100

    Uninitialized variable

--- core/class/livebox.class.php
+++ core/class/livebox.class.php
@@ -46,6 +46,7 @@ class livebox extends eqLogic {
 	}
 	public static function addFavorite($num,$name) {
 		$favoris = config::byKey('favorites','livebox',array());
+        $found = false;
 		foreach ($favoris as $favori) {
 			if($favori['phone'] == $num){
 				$found = true;
@@ -1483,8 +1484,11 @@ class livebox extends eqLogic {
 			if(strlen($num) == 10) {
 				$fmt = substr($num,0,2) .' '.substr($num,2,2) .' '.substr($num,4,2) .' '.substr($num,6,2) .' '.substr($num,8);
 				$usepagesjaunes = config::byKey('pagesjaunes','livebox', false);
-				if($usepagesjaunes == 1) return($fmt);
-				else return("<a target=_blank href=\"https://www.pagesjaunes.fr/annuaireinverse/recherche?quoiqui=".$num."&proximite=0\">".$fmt."</a>");
+				if($usepagesjaunes == 1) {
+                    return($fmt);
+				} else {
+                    return("<a target=_blank href=\"https://www.pagesjaunes.fr/annuaireinverse/recherche?quoiqui=".$num."&proximite=0\">".$fmt."</a>");
+                }
 			}
 		}
 		return($num);
